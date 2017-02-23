ec6b97045f41f9ec0956afdcf5f9d7e2501da39f
Tina Holly <tinacious.design@gmail.com>
2017-02-22 23:34:18 -0500

    solution

--- fridge.js
+++ fridge.js
@@ -2,13 +2,37 @@ var tastyFoods = [ 'tacos', 'pizza', 'burgers', 'salad' ];
 var healthyFoods = [ 'celery', 'carrots', 'kale', 'kohlrabi', 'brussels sprouts' ];
 
 // See README for instructions
-function fillTheFridge(foodList) {
-  // ...
+
+var fridge = document.getElementById('fridge');
+
+function fillTheFridge(foodList) {  
+  
+  for (var i = 0; i < foodList.length; i++) {
+  
+    // Get the food item
+    var foodItem = foodList[i];
+
+    // Create the food item element
+    var foodItemElement = document.createElement('div');
+
+    // Put the food in the element, e.g. tupperware
+    foodItemElement.innerText = foodItem;
+
+    // Add the class for styling
+    foodItemElement.classList.add('fridge__item');
+
+    /// Add the tupperware of food to the fridge
+    fridge.appendChild(foodItemElement);
+  }
 }
 
+
 // Run it!
+fillTheFridge(tastyFoods);
 
 
 // Now try the bonus!
 
+// var allTheFoods = healthyFoods.concat(tastyFoods);
 
+// fillTheFridge(allTheFoods);
