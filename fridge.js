5f981a2b83682d21d1aa4834d1630f9c989da3a7
Tina Holly <tinacious.design@gmail.com>
2017-02-22 23:25:27 -0500

    rm instructions from fridge.js

--- fridge.js
+++ fridge.js
@@ -1,18 +1,7 @@
 var tastyFoods = [ 'tacos', 'pizza', 'burgers', 'salad' ];
 var healthyFoods = [ 'celery', 'carrots', 'kale', 'kohlrabi', 'brussels sprouts' ];
 
-/*
-  INSTRUCTIONS
-
-  Write a function that takes an array of strings (food items)
-  and adds them to the fridge.
-
-  Requirements:
-    - You will need to find the fridge
-    - You'll need to iterate over the food items with a loop or array method
-    - You'll need to add a class of fridge__item to the food so it displays properly
-    - Don't forget to run the function
-*/
+// See README for instructions
 function fillTheFridge(foodList) {
   // ...
 }
@@ -20,13 +9,6 @@ function fillTheFridge(foodList) {
 // Run it!
 
 
-
-/*
-  BONUS: 
-    Add both food items to the fridge by combining both food arrays 
-    (and calling the function only once!)
- */
-
-
+// Now try the bonus!
 
 
